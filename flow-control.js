b31412acef0f075b67c350bbc1c5eb65913b690d
Aiyanna Tiangco <freedolphin.haru@gmail.com>
2020-01-16 07:27:12 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,18 @@
 function basicTeenager(age) {
-
+  if(age){
+    //age should be between 13 & 19 but not more than 19
+    13 <= 19
+    console.log('')
+  }
 }
 
 function teenager(age) {
-
+  if(age){
+    13 <= 191
+    console.log('You are a teenager!')
+  } else {
+    console.log('You are not a teenager')
+  }
 }
 
 function ageChecker(age) {
@@ -11,9 +20,52 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
+  return (age ? 
 
 }
 
 function switchAge(age) {
+  switch(age){
+      case "13":
+        console.log("You are a teenager");
+          default: 
+          console.log("You have an age");
+          break;
 
+          case "14":
+            console.log("You are a teenager");
+              default: 
+              console.log("You have an age");
+              break;
+
+          case "15":
+            console.log("You are a teenager");
+            default: 
+                console.log("You have an age");
+                break;
+
+          case "16":
+            console.log("You are a teenager");
+            default: 
+            console.log("You have an age");
+            break;
+
+          case "17":
+            console.log("You are a teenager");
+              default: 
+              console.log("You have an age");
+              break;
+
+          case "18":
+            console.log("You are a teenager");
+            default: 
+              console.log("You have an age");
+              break;
+
+          case "19":
+                console.log("You are a teenager");
+                  default: 
+                  console.log("You have an age");
+                  break;
+  }
 }
