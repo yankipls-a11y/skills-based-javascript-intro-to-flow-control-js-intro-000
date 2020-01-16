73e01abed03ac0a9d150d1fafb43bd7e26baff51
Aiyanna Tiangco <freedolphin.haru@gmail.com>
2020-01-16 09:05:27 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,7 +1,7 @@
 function basicTeenager(age) {
   if(age == 13 <= 19){
     //age should be between 13 & 19 but not more than 19
-    console.log('You are a teenager!')
+    console.log('You are a teenager!');
   }
 }
 
